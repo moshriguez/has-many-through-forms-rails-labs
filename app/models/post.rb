4ef7191716b5143b3abad0eaa9351124e4eb18b5
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-14 16:34:54 -0700

    commit tree 0d92daee323189cec48d126168d928a304a6808b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
   has_many :comments
   has_many :users, through: :comments
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+p = Post.create(title: 'Fun', content: 'So much fun...')
+u = User.create(username: 'hahaha36', email: 'haha@email.com')
+c = Comment.create(content: 'such a good post', user: u, post: p)
+cat = Category.create(name: 'best')
+pc = PostCategory.create(post: p, category: cat)
